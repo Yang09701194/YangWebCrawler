71e87f77410ea36a465ee735014c2d1494956eef
Yang09701194 <mustnecessity@gmail.com>
2020-03-08 15:39:24 +0800

    Add Unit Test Project

---
+++ YangWebCrawler/Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YangWebCrawler.DataAccessLayer;
+
+namespace Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			WebHelper.DownloadPage();
+
+			Assert.AreEqual((object)1, (object)1);
+		}
+	}
+}
